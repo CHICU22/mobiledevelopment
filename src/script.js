60db5bee7f80948f1b1a11a6170f4e5eb3821cfd
CHICU22 <alexandrevieru08@gmail.com>
2023-02-22 20:56:31 +0000

    functions on buttons

--- src/script.js
+++ src/script.js
@@ -1,7 +1,35 @@
-import SliderBar from "./components/SlideBar.js";
+import Button from "./components/Button.js";
+import RangeBar from "./components/RangeBar.js";
+import ToggleButton from "./components/ToggleButton.js";
 
 window.onload=() => {
-  const slideBar = new SliderBar("#volume");
 
-  const scrub = new SliderBar("#scrub")
+  const previousButton = new Button("#previous-button");
+  previousButton.onClick = (value) => {
+
+    console.log('previous button', value)
+  }
+
+  const button = new ToggleButton("#action-button");
+  button.onClick = (value) => {
+    console.log('toggle button', value);
+    button.toggle(1);
+  }
+  const slideBar = new RangeBar("#volume");
+
+  const menuButton = new ToggleButton("#menu-button");
+  menuButton.onClick = (value) => {
+    console.log("menu button", value)
+    menuButton.toggle();
+  }
+
+  const infoButton = new ToggleButton("#info-button");
+  infoButton.onClick = (value) => {
+    console.log("info button", value)
+    infoButton.toggle();
+  }
+
+  infoButton.toggle(0);
+  menuButton.toggle(0);
+  button.toggle(0);
 };
