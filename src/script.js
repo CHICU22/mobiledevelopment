68dcdb46b6e033a1e20536b24526d454b3d4def9
CHICU22 <alexandrevieru08@gmail.com>
2023-02-23 20:58:36 +0000

    some changes on code

--- src/script.js
+++ src/script.js
@@ -1,35 +1,42 @@
 import Button from "./components/Button.js";
+import Menu from "./components/Menu.js";
 import RangeBar from "./components/RangeBar.js";
 import ToggleButton from "./components/ToggleButton.js";
 
-window.onload=() => {
+window.onload = () => {
+
+  setupLayout();
+}
+
+const setupLayout = () => {
+
+  const volumeBar = new RangeBar("#volume");
+  volumeBar.onChange((value) =>{
+    console.log("volume changed", value)
+  });
 
   const previousButton = new Button("#previous-button");
-  previousButton.onClick = (value) => {
-
+  previousButton.onClick((value) => {
     console.log('previous button', value)
-  }
+  }) 
+
+  const nextButton = new Button("#next-button");
+  nextButton.onClick((value) => {
+    console.log('next button', value)
+  }) 
 
   const button = new ToggleButton("#action-button");
-  button.onClick = (value) => {
+  button.onClick((value) => {
     console.log('toggle button', value);
     button.toggle(1);
-  }
-  const slideBar = new RangeBar("#volume");
-
-  const menuButton = new ToggleButton("#menu-button");
-  menuButton.onClick = (value) => {
-    console.log("menu button", value)
-    menuButton.toggle();
-  }
+  })
 
   const infoButton = new ToggleButton("#info-button");
-  infoButton.onClick = (value) => {
+  infoButton.onClick((value) => {
     console.log("info button", value)
     infoButton.toggle();
-  }
+  })
 
-  infoButton.toggle(0);
-  menuButton.toggle(0);
-  button.toggle(0);
-};
+  const menu = new Menu("#menu");
+}
+
